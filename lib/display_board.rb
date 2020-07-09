440219e0133f8238702fc6664d74efc165c43ba3
James P Bondeson <jpbond58@gmail.com>
2020-07-09 03:09:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,7 @@
 def display_board
-  puts "A Tic Tac Toe Board"
- "   |   |   "
- "-----------"
- "   |   |   "
- "-----------"
- "   |   |   "
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
 end
-
-print display_board
